68d3f2123f53c329dca9407be782391a2dc44143
PSW <sorlti6952@gmail.com>
2016-10-20 14:49:08 +0900

    2016 10 20

--- app/src/main/java/com/industrial/subway/MainActivity.java
+++ app/src/main/java/com/industrial/subway/MainActivity.java
@@ -1,13 +1,57 @@
 package com.industrial.subway;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView btnShowmap;
+    private MaterialEditText etEnter;
+    private MaterialEditText etExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initEvent();
+    }
+
+    private void initEvent() {
+        btnShowmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        etEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        etExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void initView() {
+        btnShowmap = (TextView) findViewById(R.id.btn_show_map);
+        etEnter = (MaterialEditText) findViewById(R.id.et_enter);
+        etExit = (MaterialEditText) findViewById(R.id.et_exit);
     }
 }
